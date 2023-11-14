0087080ffeec3542f3b223c8fda7c658e76dbc9f
bendgk <benkosten@gmail.com>
2023-11-14 10:18:59 -0500

    initial commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,96 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import { GLView } from 'expo-gl';
+import {PanResponder, StyleSheet, View} from 'react-native';
+import { useRef } from 'react';
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    width: '100%',
+    height: '100%',
   },
 });
+
+export default function App() {
+  const panResponder = useRef(PanResponder.create({
+    // Ask to be the responder:
+    onStartShouldSetPanResponder: (evt, gestureState) => true,
+    onStartShouldSetPanResponderCapture: (evt, gestureState) => true,
+    onMoveShouldSetPanResponder: (evt, gestureState) => true,
+    onMoveShouldSetPanResponderCapture: (evt, gestureState) => true,
+    onPanResponderGrant: (evt, gestureState) => {
+      // The gesture has started. Show visual feedback so the user knows
+      // what is happening!
+      // gestureState.d{x,y} will be set to zero now
+    },
+    onPanResponderMove: (evt, gestureState) => {
+      // The most recent move distance is gestureState.move{X,Y}
+      // The accumulated gesture distance since becoming responder is
+      // gestureState.d{x,y}
+
+      console.log(evt.nativeEvent.touches.length)
+    },
+    onPanResponderTerminationRequest: (evt, gestureState) =>
+      true,
+    onPanResponderRelease: (evt, gestureState) => {
+      // The user has released all touches while this view is the
+      // responder. This typically means a gesture has succeeded
+    },
+    onPanResponderTerminate: (evt, gestureState) => {
+      // Another component has become the responder, so this gesture
+      // should be cancelled
+    },
+    onShouldBlockNativeResponder: (evt, gestureState) => {
+      // Returns whether this component should block native components from becoming the JS
+      // responder. Returns true by default. Is currently only supported on android.
+      return true;
+    },
+  }))
+
+  function onContextCreate(gl) {
+    gl.viewport(0, 0, gl.drawingBufferWidth, gl.drawingBufferHeight);
+    gl.clearColor(0, 1, 1, 1);
+
+    // Create vertex shader (shape & position)
+    const vert = gl.createShader(gl.VERTEX_SHADER);
+    gl.shaderSource(
+      vert,
+      `
+      void main(void) {
+        gl_Position = vec4(0.0, 0.0, 0.0, 1.0);
+        gl_PointSize = 150.0;
+      }
+    `
+    );
+    gl.compileShader(vert);
+
+    // Create fragment shader (color)
+    const frag = gl.createShader(gl.FRAGMENT_SHADER);
+    gl.shaderSource(
+      frag,
+      `
+      void main(void) {
+        gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);
+      }
+    `
+    );
+    gl.compileShader(frag);
+
+    // Link together into a program
+    const program = gl.createProgram();
+    gl.attachShader(program, vert);
+    gl.attachShader(program, frag);
+    gl.linkProgram(program);
+    gl.useProgram(program);
+
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    gl.drawArrays(gl.POINTS, 0, 1);
+
+    gl.flush();
+    gl.endFrameEXP();
+  }
+
+  return (
+    <View style={styles.container}>
+      <GLView style={styles.container} onContextCreate={onContextCreate} {...panResponder.current.panHandlers} />
+    </View>
+  )
+}
